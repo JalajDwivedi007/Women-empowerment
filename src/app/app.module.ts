a7a698bd401352b80776fb234995cb8d1f9c3ca5
Jalaj Dwivedi <jalajdwivedi2001@gmail.com>
2022-07-17 14:18:54 +0530

    Started PRoject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
