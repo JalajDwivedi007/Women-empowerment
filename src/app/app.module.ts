b135855994acfedc09749dcfa9af4ef054c56bae
Jalaj Dwivedi <jalajdwivedi2001@gmail.com>
2022-07-21 19:22:53 +0530

    Made static data folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
-import { GuidelinesComponent } from './guidelines/guidelines.component';
-import { CriteriaComponent } from './criteria/criteria.component';
-import { PoliciesLegislationsComponent } from './policies-legislations/policies-legislations.component';
+import { GuidelinesComponent } from './static-page/guidelines/guidelines.component';
+import { CriteriaComponent } from './static-page/criteria/criteria.component';
+import { PoliciesLegislationsComponent } from './static-page/policies-legislations/policies-legislations.component';
 import { TraineeRegisterComponent } from './trainee-register/trainee-register.component';
-import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutUsComponent } from './static-page/about-us/about-us.component';
 
 @NgModule({
   declarations: [
