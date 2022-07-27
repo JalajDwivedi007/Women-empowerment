580ecbbf4037e6c1e3c97eef5d8148ef8566b4f5
Laveen Dawani <dawanilaveen@gmail.com>
2022-07-27 16:49:51 +0530

    Added admin-trainee

---
+++ src/app/admin-trainee/admin-trainee.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminTraineeComponent } from './admin-trainee.component';
+
+describe('AdminTraineeComponent', () => {
+  let component: AdminTraineeComponent;
+  let fixture: ComponentFixture<AdminTraineeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminTraineeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminTraineeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
