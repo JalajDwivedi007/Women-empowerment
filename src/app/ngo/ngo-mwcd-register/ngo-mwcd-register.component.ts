f523ecca826f8e01cc024d24be949f754c01127f
ruchika <rucha6792@gamil.com>
2022-07-30 17:50:15 +0530

    added ngo-register

--- src/app/ngo/ngo-mwcd-register/ngo-mwcd-register.component.ts
+++ src/app/ngo/ngo-mwcd-register/ngo-mwcd-register.component.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from 'src/app/login/login.component';
+import { NgoRegisterService } from 'src/app/ngo-register.service';
 
 @Component({
   selector: 'app-ngo-mwcd-register',
   templateUrl: './ngo-mwcd-register.component.html',
-  styleUrls: ['./ngo-mwcd-register.component.css']
+  styleUrls: ['./ngo-mwcd-register.component.css'],
 })
 export class NgoMwcdRegisterComponent implements OnInit {
+  ngoName: String;
+  members: Number;
+  workAreas: String;
+  ngoState: String;
+  ngocity: String;
+  ngoAddress: String;
+  ngoRegistrationNumber: Number;
+  ngoAmount: Number;
+  ngoMobile: Number;
+  ngoDOE: String;
+  newCampaign: String;
+  ngoProjDetails: String;
+  ngoEmail: String;
+  ngoWebsite: String;
+  //PROJECT IN CHARGE DETAILS
+  fname: String;
+  mname: String;
+  lname: String;
+  inchargeState: String;
+  inchargecity: String;
+  inchargeAddress: String;
+  inchargePan: String;
+  inchargeEdu: String;
+  inchargeAadhar: Number;
+  file: File;
 
-  constructor() { }
+  constructor(private ngoService: NgoRegisterService) {}
 
   ngOnInit(): void {
+    // this.getInfo();
   }
 
+  onSubmit() {
+    console.log(this.file);
+    let data = {
+      ngoName: this.ngoName,
+      members: this.members,
+      workAreas: this.workAreas,
+      ngoState: this.ngoState,
+      ngocity: this.ngocity,
+      ngoAddress: this.ngoAddress,
+      ngoRegistrationNumber: this.ngoRegistrationNumber,
+      ngoDOE: this.ngoDOE,
+      newCampaign: this.newCampaign,
+      ngoProjDetails: this.ngoProjDetails,
+      ngoAmount: this.ngoAmount,
+      ngoMobile: this.ngoMobile,
+      ngoEmail: this.ngoEmail,
+      ngoWebsite: this.ngoWebsite,
+      //Project in-charge
+      fname: this.fname,
+      mname: this.mname,
+      lname: this.lname,
+      inchargeState: this.inchargeState,
+      inchargecity: this.inchargecity,
+      inchargeAddress: this.inchargeAddress,
+      inchargePan: this.inchargePan,
+      inchargeEdu: this.inchargeEdu,
+      inchargeAadhar: this.inchargeAadhar,
+      files: this.file,
+    };
+    this.ngoService.postData(data).subscribe((res) => {
+      // console.log(res);
+      this.getInfo();
+    });
+  }
+  getInfo() {
+    this.ngoService.getData().subscribe((res) => {
+      console.log(res);
+    });
+  }
+  fileChange(event: any) {
+    // Instantiate an object to read the file content
+    let reader = new FileReader();
+    // when the load event is fired and the file not empty
+    if (event.target.files && event.target.files.length > 0) {
+      // Fill file variable with the file content
+      this.file = event.target.files[0];
+    }
+  }
 }
